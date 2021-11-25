724f7a316396dc6813f00081e526cfb498807096
Nathalia Vieira <naahviera@gmail.com>
2021-11-24 23:11:08 -0300

    2nd commit

---
+++ src/main/java/com/dh/ORM/ORM/controller/HelloWord.java
@@ -0,0 +1,12 @@
+package com.dh.ORM.ORM.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWord {
+    @GetMapping("/hello")
+    public String helloWord(){
+        return " Hello Word sou eu a Nathalia";
+    }
+}
